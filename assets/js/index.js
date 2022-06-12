78ad39a9b22a7fb746df7f926951481d796d6390
kotoriefreet <1063377232@qq.com>
2022-06-12 19:37:16 +0800

    完成文章管理相关功能的开发

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,7 +13,7 @@ function getUserInfo() {
     //   Authorization: localStorage.getItem("token"),
     // },
     success: (res) => {
-      console.log(res);
+      // console.log(res);
       if (res.status !== 0) return layer.msg("获取用户信息失败！");
       layer.msg("获取用户信息成功！");
       renderAvatar(res.data);
